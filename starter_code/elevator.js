169aced8becf82d3c1fc9d46ed9ccd209d97665a
JB-Tellez <jb.tellez@gmail.com>
2017-07-03 17:27:08 +0200

    start, stop, floorUp, floorDown

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -5,15 +5,34 @@ class Elevator {
     this.requests   = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    this.intervalID = setInterval( () => this.update(), 1000);
+   }
+  stop() { 
+    if (this.intervalID) {
+      clearInterval(this.intervalID);
+      this.intervalID = undefined;
+    }
+  }
+  update() {
+    this.log();
+   }
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
+  floorUp() { 
+    if (this.floor < this.MAXFLOOR) {
+      this.floor++;
+    }
+  }
+  floorDown() { 
+    if (this.floor > 0) {
+      this.floor--;
+    }
+  }
   call() { }
-  log() { }
+  log() { 
+    console.log(`Direction: up | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
